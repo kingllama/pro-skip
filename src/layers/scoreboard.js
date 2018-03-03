5b0eb8883ff778034a02b0d2a2785dcfcfb48972
Daniel Harris <daniel@spocket.co>
2018-03-03 00:02:21 -0800

    cleaned up scoreboard

--- src/layers/scoreboard.js
+++ src/layers/scoreboard.js
@@ -8,18 +8,14 @@ export default class ScoreBoardLayer extends LayerBase {
     this.drawRectangle(scoreBoardOrigin,scoreBoardEnd,'#2e3fab')
     const team1Y = 40
     const team2Y = 65
+
     this.drawStones(team1, team1Y - 6)
     this.drawStones(team2, team2Y - 6)
-    this.drawText(`team1 - ${team1.score}`,new Point(25,team1Y))
-    this.drawText(`team2 - ${team2.score}`,new Point(25,team2Y))
+
+    this.drawText(`team1 - ${team1.score} ${team1.hasLastStone ? '✓' : ''}`,new Point(25,team1Y))
+    this.drawText(`team2 - ${team2.score} ${team2.hasLastStone ? '✓' : ''}`,new Point(25,team2Y))
 
     this.drawText(`end ${currentEnd}`,new Point(25,90))
-
-    if(team1.hasLastStone){
-      this.drawHammer(108, team1Y)
-    } else {
-      this.drawHammer(108, team2Y)
-    }
   }
 
   drawStones(team, y){
@@ -27,11 +23,5 @@ export default class ScoreBoardLayer extends LayerBase {
     for (let i = 0; i < team.stonesRemainingThisEnd; i++) {
       this.drawCircle(new Point(130 + i*stoneSpacing, y), 5, team.color)
     }
-  }
-
-  drawHammer(x, y){
-    const origin = new Point(x, y)
-    const end = new Point(x + 5, y + 10)
-    this.drawText('✓',origin)
   }
 }
